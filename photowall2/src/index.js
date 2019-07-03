e60c163a48dd8eb7502b86c91d83fe6e4a4525f7
Snehal <m.snehal106@gmail.com>
2019-07-02 20:30:33 -0400

    Gallery

---
+++ photowall2/src/index.js
@@ -0,0 +1,6 @@
+import React, {Component} from 'react';
+import ReactDOM from 'react-dom';
+import Main from './Components/Main'
+import './Styles/stylesheet.css'
+
+ReactDOM.render(<Main/>,document.getElementById('root'));
