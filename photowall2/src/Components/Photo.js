0df8cae5c7f178e9f49e5a7182ef788a73c5cfea
Snehal <m.snehal106@gmail.com>
2019-07-02 22:00:33 -0400

    State management

--- photowall2/src/Components/Photo.js
+++ photowall2/src/Components/Photo.js
@@ -6,7 +6,9 @@ function Photo(props){
   <img className="photo" src={post.imageLink} alt={post.description}/>
   <figcaption><p>{post.description}</p></figcaption>
   <div className="button-container">
-  <button className="remove-button">Remove</button>
+  <button className="remove-button"onClick={() =>{
+    props.onRemovePhoto
+  }}>Remove</button>
   </div>
   </figure>
 
