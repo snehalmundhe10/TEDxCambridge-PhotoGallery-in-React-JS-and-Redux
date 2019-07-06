9b95e5dab1b091a2c3b2804cb3c3c26545685022
Snehal <m.snehal106@gmail.com>
2019-07-06 15:43:42 -0400

    Installed react-router-dom package

--- photowall2/src/index.js
+++ photowall2/src/index.js
@@ -2,5 +2,6 @@ import React, {Component} from 'react';
 import ReactDOM from 'react-dom';
 import Main from './Components/Main'
 import './Styles/stylesheet.css'
+import {BrowserRouter} from 'react-router-dom'
 
 ReactDOM.render(<Main/>,document.getElementById('root'));
