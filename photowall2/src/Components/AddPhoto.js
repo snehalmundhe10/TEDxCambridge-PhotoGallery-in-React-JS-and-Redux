cbc200c222a0be2000eb41020006d8447fda53f0
Snehal <m.snehal106@gmail.com>
2019-07-06 21:01:53 -0400

    Styling

--- photowall2/src/Components/AddPhoto.js
+++ photowall2/src/Components/AddPhoto.js
@@ -2,7 +2,18 @@ import React,{Component} from 'react'
 
 class AddPhoto extends Component{
   render(){
-     return <h1>This is the page where we will add photos</h1>
+     return (
+    <div>
+     <h1>Add Photo</h1>
+     <div className="form">
+     <form>
+     <input type="text" placeholder="Link"/>
+     <input type="text" placeholder="Description"/>
+     <button>Post</button>
+     </form>
+     </div>
+    </div>
+   )
   }
 }
 export default AddPhoto
