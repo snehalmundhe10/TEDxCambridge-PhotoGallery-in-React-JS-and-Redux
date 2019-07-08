6e41d323a13ce762cfa03d90af2b92810b38e96f
Snehal <m.snehal106@gmail.com>
2019-07-07 23:10:39 -0400

    redux

--- photowall2/src/index.js
+++ photowall2/src/index.js
@@ -3,5 +3,8 @@ import ReactDOM from 'react-dom';
 import Main from './Components/Main'
 import './Styles/stylesheet.css'
 import {BrowserRouter} from 'react-router-dom'
+import {createStore} from 'redux'
+
+const store=createStore();
 
 ReactDOM.render(<BrowserRouter><Main/></BrowserRouter>,document.getElementById('root'));
