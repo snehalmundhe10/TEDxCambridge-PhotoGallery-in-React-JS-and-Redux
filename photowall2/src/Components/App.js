dd4106e7a91ba47a6252160b5d15395a2eec327e
Snehal <m.snehal106@gmail.com>
2019-07-08 02:30:42 -0400

    comments about redux

--- photowall2/src/Components/App.js
+++ photowall2/src/Components/App.js
@@ -1,10 +1,15 @@
 import Main from './Main'
 import {connect} from 'react-redux'
-function mapStateToProps(state{
+function mapStateToProps(state){
    return {
      posts:state
    }
 }
-
+{/*App os the snapshot or copy of the Main component, Main receives the prop-(posts)
+  and the connect method actually does not modify the Main component with props
+  it just connects the Main component to the connected component created by the redux/generates
+  its connected component copy that connects it to the store of the redux-so basically App is the
+  clone of the Main component which has assigned the props-but the App is 'connected component'-component of
+  redux*/}
 const App=connect(mapStateToProps)(Main)
 export default App
