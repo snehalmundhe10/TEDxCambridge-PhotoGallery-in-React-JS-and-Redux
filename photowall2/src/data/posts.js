d2bf80c03d7927ba12bca06bad1f35b9c8db71d4
Snehal <m.snehal106@gmail.com>
2019-07-09 00:06:26 -0400

    posts

--- photowall2/src/data/posts.js
+++ photowall2/src/data/posts.js
@@ -1,21 +1,16 @@
-const posts =[{
- id: 0,
- description: " ",
- imageLink: "https://pi.tedcdn.com/r/pe.tedcdn.com/images/ted/61f4f84fa3604cd3d321fa15a767696947a496e6_1600x1200.jpg?quality=90&w=260"
+const posts = [{
+ id: "0",
+ description: "beautiful landscape",
+ imageLink: "https://image.jimcdn.com/app/cms/image/transf/none/path/sa6549607c78f5c11/image/i4eeacaa2dbf12d6d/version/1490299332/most-beautiful-landscapes-in-europe-lofoten-european-best-destinations-copyright-iakov-kalinin.jpg" +
+ "3919321_1443393332_n.jpg"
  }, {
- id: 1,
- description: " ",
- imageLink: "https://mk0atomicdesign69dg3.kinstacdn.com/wp-content/uploads/2018/04/TEDx-Cambridge-2018_Cirrus_Udon_Fog_a0bb.jpg"
+ id: "1",
+ description: "Aliens???",
+ imageLink: "https://img.purch.com/rc/640x415/aHR0cDovL3d3dy5zcGFjZS5jb20vaW1hZ2VzL2kvMDAwLzA3Mi84NTEvb3JpZ2luYWwvc3BhY2V4LWlyaWRpdW00LWxhdW5jaC10YXJpcS1tYWxpay5qcGc=" +
+ "08323785_735653395_n.jpg"
  }, {
- id: 2,
- description: " ",
- imageLink: "https://pi.tedcdn.com/r/pe.tedcdn.com/images/ted/61f4f84fa3604cd3d321fa15a767696947a496e6_1600x1200.jpg?quality=90&w=260"
-},
-{
-id: 3,
-description: " ",
-imageLink: "https://pbs.twimg.com/media/D5tonfiWkAAaW4N.jpg"
-}]
-
-
+ id: "2",
+ description: "On a vacation!",
+ imageLink: "https://fm.cnbc.com/applications/cnbc.com/resources/img/editorial/2017/08/24/104670887-VacationExplainsTHUMBWEB.1910x1000.jpg"
+ }]
 export default posts;
