56e52e88e697cf1aa122c100bdcceac09ba57b9e
Snehal <m.snehal106@gmail.com>
2019-07-09 00:05:30 -0400

    photowall

--- photowall2/src/Components/PhotoWall.js
+++ photowall2/src/Components/PhotoWall.js
@@ -1,33 +1,19 @@
-import React from 'react'
-import PropTypes from 'prop-types'
+import React, { Component } from 'react';
 import Photo from './Photo'
-import {Link} from 'react-router-dom'
-
-
-function PhotoWall(props){
-  return <div>
-
-  {/*<button onClick={props.onNavigate} className="addIcon"></button>*/}
-  <div className="photoGrid">
-  {props.posts.sort(function(x,y){
-    return y.id-x.id
-  })
-
-    .map((post,index)=><Photo key={index} post={post} {...props} index={index}/>)}
-
-  </div>
-
-  <Link to="/AddPhoto">
-  <button className="addMorePhotos">Add More Photos</button>
-  </Link>
-  </div>
+import { Link } from 'react-router-dom'
+class PhotoWall extends Component {
+ render() {
+ return (
+ <div>
+ <Link to='/create' className="addpicture"></Link>
+ <div className="photo-grid">
+ {console.log(this.props)}
+ {this.props.posts.sort(function(x, y){return y.id - x.id;}).map((post, i) => {
+ return <Photo key={i} post={post} i={i} {...this.props}/>
+ })}
+ </div>
+ </div>
+ );
+ }
 }
-
-PhotoWall.propTypes={
-  posts:PropTypes.array.isRequired,
-
-}
-
-
-
-export default PhotoWall
+export default PhotoWall;
