584ac362ea722c29c5502bf48e24d7c1933c7c9e
Snehal <m.snehal106@gmail.com>
2019-07-09 00:03:48 -0400

    main

--- photowall2/src/Components/Main.js
+++ photowall2/src/Components/Main.js
@@ -1,45 +1,21 @@
 import React, {Component} from 'react';
-import Title from './Title'
-import PhotoWall from './PhotoWall'
-import AddPhoto from './AddPhoto'
-import {Route} from 'react-router-dom'
-import {removePost} from  '../redux/actions'
-
-
-class Main extends Component{
-  constructor(props){
-    super(props);
-    console.log('constructor')
-     
-  }
-
-  // componentDidMount()
-  //  {
-  //   this.props.removePost(1)
-  //
-  // }
-
-
-   render(){
-
-     return (
-    <div>
-     <Route exact path="/" render={() => (
-       <div>
-       <Title title={'TEDxCambridge'}/>
-       {/*<PhotoWall posts={this.state.posts} onRemovePhoto={this.removePhoto} onNavigate={this.navigate}/>*/}
-       <PhotoWall {...this.props}/>
-       </div>
-     )}/>
-
-       <Route path= "/AddPhoto" render = {({history}) => (
-  <AddPhoto {...this.props} onHistory={history}/>
-    )}/>
-
-    </div>
-   )
-   }
+import {Link} from 'react-router-dom'
+import PhotoWall from '../Components/PhotoWall'
+import AddPost from '../Components/AddPost'
+import { Route,Switch } from 'react-router-dom'
+class Main extends Component {
+ render() {
+ return (
+ <div>
+ <h1 className="font-face">
+ <Link to="/">Photowall</Link>
+ </h1>
+ <Switch>
+ <Route exact path='/' render={(params) => (<PhotoWall {...this.props}{...params}/>)}/>
+ <Route path='/create' render={(params) => (<AddPost {...this.props}{...params}/>)}/>
+ </Switch>
+ </div>
+ );
+ }
 }
-
-
-export default Main
+export default Main;
