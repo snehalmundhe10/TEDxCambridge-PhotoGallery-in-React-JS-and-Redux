3cc464c9acdbea0704b3ac0b71e7ca6bbaa80d79
Snehal <m.snehal106@gmail.com>
2019-07-09 00:08:22 -0400

    index

--- photowall2/src/index.js
+++ photowall2/src/index.js
@@ -1,12 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './Styles/stylesheet.css'
-import {BrowserRouter} from 'react-router-dom'
-import {createStore} from 'redux'
-import rootReducer from './redux/reducer'
-import {Provider} from 'react-redux'
 import App from './Components/App'
-
-const store=createStore(rootReducer)
-
-ReactDOM.render(<Provider store={store}><BrowserRouter><App/></BrowserRouter></Provider>,document.getElementById('root'));
+import { BrowserRouter } from 'react-router-dom'
+import './styles/stylesheet.css'
+import {Provider} from 'react-redux'
+import { createStore } from 'redux';
+import rootReducer from './redux/reducer';
+const store = createStore(rootReducer)
+ReactDOM.render(<Provider store={store}>
+ <BrowserRouter>
+ <App/>
+ </BrowserRouter>
+ </Provider>,
+ document.getElementById('root'));
